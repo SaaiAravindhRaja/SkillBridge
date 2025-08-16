c3deb70dcecee75901f0a701f593d3b084f99270
Saai Aravindh Raja <saaiaravindhraja@gmail.com>
2025-08-16 21:04:43 +0800

    edit serverindex

--- server/models/index.js
+++ server/models/index.js
@@ -1,12 +1,14 @@
 const User = require('./User');
 const HelpRequest = require('./HelpRequest');
 const Session = require('./Session');
+const Message = require('./Message');
 
 // Define associations
 User.hasMany(HelpRequest, { foreignKey: 'kidId', as: 'helpRequests' });
 User.hasMany(HelpRequest, { foreignKey: 'volunteerId', as: 'volunteerRequests' });
 User.hasMany(Session, { foreignKey: 'kidId', as: 'kidSessions' });
 User.hasMany(Session, { foreignKey: 'volunteerId', as: 'volunteerSessions' });
+User.hasMany(Message, { foreignKey: 'senderId', as: 'messages' });
 
 HelpRequest.belongsTo(User, { foreignKey: 'kidId', as: 'kid' });
 HelpRequest.belongsTo(User, { foreignKey: 'volunteerId', as: 'volunteer' });
@@ -15,9 +17,14 @@ HelpRequest.hasOne(Session, { foreignKey: 'requestId', as: 'session' });
 Session.belongsTo(HelpRequest, { foreignKey: 'requestId', as: 'request' });
 Session.belongsTo(User, { foreignKey: 'kidId', as: 'kid' });
 Session.belongsTo(User, { foreignKey: 'volunteerId', as: 'volunteer' });
+Session.hasMany(Message, { foreignKey: 'sessionId', as: 'sessionMessages' });
+
+Message.belongsTo(Session, { foreignKey: 'sessionId', as: 'session' });
+Message.belongsTo(User, { foreignKey: 'senderId', as: 'sender' });
 
 module.exports = {
   User,
   HelpRequest,
-  Session
+  Session,
+  Message
 };
